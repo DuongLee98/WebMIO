bddde5fde1406c686cdc472f917559bc411145b2
Dương Lê Bình <duong98.lebinh@gmail.com>
2020-06-09 10:14:02 +0700

    UI

--- src/java/control/CategoryDAOImpl.java
+++ src/java/control/CategoryDAOImpl.java
@@ -6,7 +6,13 @@
 package control;
 
 import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.ArrayList;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import model.Category;
 
 /**
  *
@@ -20,12 +26,39 @@ private Connection connection;
     }
     @Override
     public ArrayList getAll() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        ArrayList<Category> rs = new ArrayList<>();
+        
+        String sql="SELECT * FROM category";
+        try {
+            PreparedStatement ps = connection.prepareStatement(sql);
+            ResultSet rss = ps.executeQuery();
+            while(rss.next())
+            {
+                Category obj = new Category();
+                obj.setId(rss.getInt("Id"));
+                obj.setName(rss.getString("Name"));
+                
+                //account.setPersonId(personId);
+                rs.add(obj);
+            }
+        } catch (SQLException ex) {
+            Logger.getLogger(AccountDAOImpl.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return rs;
     }
 
     @Override
     public void add(Object t) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        Category in = (Category) t;
+        String sql = "Insert into category (name) values (?)";
+        
+        try {
+            PreparedStatement ps = connection.prepareStatement(sql);
+            ps.setString(1, in.getName());
+            ps.execute();
+        } catch (SQLException ex) {
+            Logger.getLogger(CategoryDAOImpl.class.getName()).log(Level.SEVERE, null, ex);
+        }
     }
 
     @Override
